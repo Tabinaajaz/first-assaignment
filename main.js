954fb8142811f7296cd8f145b2d9bed122e38a93
tabinda ajaz <159224662+Tabinaajaz@users.noreply.github.com>
2024-03-03 10:01:15 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+//string
+var personname = "tabinda";
+console.log(personname);
+//nulls
+var home = null;
+console.log(home);
+//boolean
+var ilikebook = true;
+console.log(ilikebook);
+//Number
+var age = 24;
+console.log(age);
+//undefined
+var phonenumber = undefined;
+console.log(phonenumber);
---
+++ main.ts
@@ -0,0 +1,24 @@
+//string
+let personname : string = "tabinda"
+console.log(personname);
+
+//nulls
+let home : null = null
+console.log(home);
+
+//boolean
+let ilikebook : boolean = true
+console.log(ilikebook);
+
+//Number
+let age : number = 24
+console.log(age);
+
+//undefined
+let phonenumber : undefined = undefined
+console.log(phonenumber);
+
+
+
+
+
